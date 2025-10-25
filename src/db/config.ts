ff6153b3a138ac2b215c0910d2957212d948084c
Raj Mangukiya <rajmangukiya00@gmail.com>
2025-10-25 18:20:11 +0530

    ssl config updated

--- src/db/config.ts
+++ src/db/config.ts
@@ -8,4 +8,7 @@ export const db = new Client({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: process.env.NODE_ENV === 'production' ? {
+        rejectUnauthorized: false
+    } : false,
 });
